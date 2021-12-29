191dbcaaa16359344931e1173c6c4794109d826c
yassineelhoubi <elhoubiyassine@gmail.com>
2021-12-29 16:56:07 +0100

    setup firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import "./firebase/firebaseInit.js"
 createApp(App).use(store).use(router).mount('#app')
 import "bootstrap/dist/js/bootstrap.js"
+
