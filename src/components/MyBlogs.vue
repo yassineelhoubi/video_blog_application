678f909583af6851e42b435a8b61512c9292140f
yassineelhoubi <elhoubiyassine@gmail.com>
2021-12-31 19:48:22 +0100

    dashboard template

---
+++ src/components/MyBlogs.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="../assets/img/hero-bg.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/img/hero-bg.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/img/hero-bg.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../assets/img/hero-bg.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyBlogs",
+};
+</script>
+
+<style lang="scss">
+.card {
+  .card-img-top {
+    height: 200px;
+    object-fit: cover;
+  }
+  .card-body {
+    .card-title {
+      height: 30px;
+      overflow: hidden;
+    }
+    .card-text {
+      height: 110px;
+      overflow: hidden;
+    }
+  }
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,25 @@
 <template>
-  <div id="myBlogs">
-      <h1>hello</h1>
-    <h1>This is an myBlogs page</h1>
+  <div id="myBlogs" class="pt-5 pb-3">
+    <h1>Hello to Your Dashboard</h1>
+    <div class="container mb-5 d-flex justify-content-center">
+      <button class="btn btn-primary">Create New Blog</button>
+    </div>
+    <div class="container">
+      <MyBlogs />
+    </div>
   </div>
 </template>
 
 
 <script>
+import MyBlogs from "@/components/MyBlogs.vue";
 export default {
   name: "Dashboard",
-}
+  components: {
+    MyBlogs,
+  },
+};
 </script>
 
 <style>
-
 </style>
